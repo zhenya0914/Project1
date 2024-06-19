4659f6398507e2c944cf8250361ca8128ff6270b
barixx <qwertyjinx@mail.ru>
2024-06-20 00:28:25 +0500

    1233213

--- project1/src/app/page.js
+++ project1/src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Амир ПИДАРАС&nbsp;
+          Амир красавчик&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
